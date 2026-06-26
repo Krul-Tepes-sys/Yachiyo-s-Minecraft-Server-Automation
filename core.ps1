eb8378c462fe72ee7a59bd37c80c2038a46ee547
Krul_Tepes-sys <1101980763@qq.com>
2026-06-26 23:13:09 +0800

    # 添加 ## 核心 - 配置文件导入和异常处理 ## Json解析错误帮助文档 # 修改 ## 防无限死机帮助文档 - 删除了多余的冒号

--- core.ps1
+++ core.ps1
@@ -4,4 +4,17 @@ Yachiyo's Minecraft Server Automation (YMSA)
 ============================================
 '@
 
+. "$($PSScriptRoot)\ymsa_module\function_new_makestar_alarm_dialog.ps1"
+
+try {
+    $userConfigRaw = Get-Content "$($PSScriptRoot)\config.json" -Raw -ErrorAction Stop
+    $userConfig = ConvertFrom-Json $userConfigRaw -ErrorAction Stop
+}
+catch {
+    $null = New-MakeStarAlarmDialog `
+        -Level Red `
+        -Text "解析配置文件时出错" `
+        -HelpPath "$($PSScriptRoot)\ymsa_module\json_error_alarm_help.txt"
+}
+
 $null = Read-Host "Press ENTER to Exit"
