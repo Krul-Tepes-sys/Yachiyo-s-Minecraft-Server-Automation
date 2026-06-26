bb831413f747ad8da81e32435c0f95282b900e3b
Krul_Tepes-sys <1101980763@qq.com>
2026-06-26 22:19:00 +0800

    # 添加 - 异步弹窗函数 - 防无限死机弹窗脚本

---
+++ ymsa_module/function_new_async_notice.ps1
@@ -0,0 +1,15 @@
+﻿function New-AsyncNotice {
+    param (
+        [string]$ScriptPath,
+        [switch]$UsePwSh
+    )
+    if ($UsePwSh) {$psFileName = "pwsh.exe"} else {$psFileName = "powershell.exe"} # 你真的心甘情愿用一辈子PS5.1吗？
+    Start-Process `
+        -FilePath $psFileName `
+        -WindowStyle Hidden `
+        -ArgumentList "-ExecutionPolicy RemoteSigned", "-File", $ScriptPath
+    # 我懒得写异常处理你最好别电脑都没装PS7然后你参数加个-UsePwSh
+}
+
+New-AsyncNotice -ScriptPath "$($PSScriptRoot)\safe_mode_anti_infinite_bsod_script.ps1"
+New-AsyncNotice -ScriptPath "$($PSScriptRoot)\safe_mode_anti_infinite_bsod_script.ps1" -UsePwSh
---
+++ ymsa_module/safe_mode_anti_infinite_bsod_script.ps1
@@ -0,0 +1,6 @@
+﻿. "$($PSScriptRoot)\function_new_makestar_alarm_dialog.ps1"
+New-MakeStarAlarmDialog `
+    -NoticeOnly `
+    -Level Orange `
+    -Text "因意外退出进入安全模式" `
+    -HelpPath "$($PSScriptRoot)\safe_mode_anti_infinite_bsod_help.txt"
