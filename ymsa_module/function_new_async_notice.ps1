5c6f00bf97d5f63dd4e1d941d708f06b03d5f361
Krul_Tepes-sys <1101980763@qq.com>
2026-06-26 22:22:15 +0800

    # 修复 ## 防无限死机弹窗脚本 - 删除了遗留下来的测试用代码

--- ymsa_module/function_new_async_notice.ps1
+++ ymsa_module/function_new_async_notice.ps1
@@ -10,6 +10,3 @@
         -ArgumentList "-ExecutionPolicy RemoteSigned", "-File", $ScriptPath
     # 我懒得写异常处理你最好别电脑都没装PS7然后你参数加个-UsePwSh
 }
-
-New-AsyncNotice -ScriptPath "$($PSScriptRoot)\safe_mode_anti_infinite_bsod_script.ps1"
-New-AsyncNotice -ScriptPath "$($PSScriptRoot)\safe_mode_anti_infinite_bsod_script.ps1" -UsePwSh
