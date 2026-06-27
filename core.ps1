a8024ec664dbfd9a7a649dbe4416a70ffe58f657
Krul_Tepes-sys <1101980763@qq.com>
2026-06-27 21:43:10 +0800

    # 修改 - Json配置文件解析错误弹窗现在是异步的 - 优化了Json配置文件解析错误帮助文档

--- core.ps1
+++ core.ps1
@@ -5,16 +5,13 @@ Yachiyo's Minecraft Server Automation (YMSA)
 '@
 
 . "$($PSScriptRoot)\ymsa_module\function_new_makestar_alarm_dialog.ps1"
+. "$($PSScriptRoot)\ymsa_module\function_new_async_notice.ps1"
 
 try {
-    $userConfigRaw = Get-Content "$($PSScriptRoot)\config.json" -Raw -ErrorAction Stop
+    $userConfigRaw = Get-Content "$($PSScriptRoot)\ymsa_module\user_config.json" -Raw -ErrorAction Stop
     $userConfig = ConvertFrom-Json $userConfigRaw -ErrorAction Stop
 }
 catch {
-    $null = New-MakeStarAlarmDialog `
-        -Level Red `
-        -Text "解析配置文件时出错" `
-        -HelpPath "$($PSScriptRoot)\ymsa_module\json_error_alarm_help.txt"
+    New-AsyncNotice -ScriptPath "$($PSScriptRoot)\ymsa_module\json_error_alarm_script.ps1"
+    exit
 }
-
-$null = Read-Host "Press ENTER to Exit"
---
+++ ymsa_module/json_error_alarm_script.ps1
@@ -0,0 +1,6 @@
+﻿. "$($PSScriptRoot)\function_new_makestar_alarm_dialog.ps1"
+New-MakeStarAlarmDialog `
+    -NoticeOnly `
+    -Level Red `
+    -Text "解析Json配置文件时出错" `
+    -HelpPath "$($PSScriptRoot)\json_error_alarm_help.txt"
